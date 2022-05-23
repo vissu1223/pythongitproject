9b694609d33a6d7950dc3f872929bcd01e3a5ce4
vissu1223 <105965162+vissu1223@users.noreply.github.com>
2022-05-23 09:40:12 +0530

    Update application.py

--- helloworld/application.py
+++ helloworld/application.py
@@ -7,11 +7,11 @@ application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    return Response(json.dumps({'Output': 'Hello World! source code updated'}), mimetype='application/json', status=200)
 
 @application.route('/', methods=['POST'])
 def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    return Response(json.dumps({'Output': 'Hello World  source code updated'}), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     default_port = "80"
